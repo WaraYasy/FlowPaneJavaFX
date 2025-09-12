1458fa8eb81451fa843e3a9c25afa220e4cc5385
Warita <warayasyp@gmail.com>
2025-09-12 18:53:14 +0200

    CHANGES: Se ha modificado el nombre de la aplicación y se ha editado JavaDoc

--- src/main/java/org/dein/AppFlowPane.java
+++ src/main/java/org/dein/AppFlowPane.java
@@ -9,12 +9,25 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
- * JavaFX App
+ * Clase principal de la aplicación JavaFX.
+ * <p>
+ * Se encarga de cargar la interfaz desde un archivo FXML, aplicar estilos CSS y mostrar la ventana principal.
+ * </p>
+ *
+ * @author Wara Pacheco
+ * @version 1.0
  */
 public class App extends Application {
 
     private static Scene scene;
 
+    /**
+     * Este método se ejecuta al arrancar la aplicación.
+     * Carga la interfaz (FXML), aplica el CSS y muestra la ventana.
+     *
+     * @param stage Ventana principal de la aplicación.
+     * @throws IOException Si hay un problema al cargar el archivo FXML.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/org/dein/fxml/flowPane.fxml"));
@@ -28,7 +41,12 @@ public class App extends Application {
         stage.show();
     }
 
-
+    /**
+     * Método principal de la aplicación.
+     * Lanza la aplicaación de JavaFX.
+     *
+     * @param args Argumentos de la línea de comandos (No sse requieren).
+     */
     public static void main(String[] args) {
         launch();
     }
